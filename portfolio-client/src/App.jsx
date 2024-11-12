11275b6ed56a08e26284691cf2dcc4be9a5b8b82
homonoviscoding <mama.leye@gmail.com>
2024-11-12 10:36:02 +0000

    modified components folder structure, added css files

--- portfolio-client/src/App.jsx
+++ portfolio-client/src/App.jsx
@@ -1,22 +1,21 @@
-import { useState } from 'react'
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+// import { useState } from 'react'
 import './App.css'
+import './App.css'
+import Sidebar from './components/sidebar/Sidebar'
+import Feed from './components/feed/Feed'
+import Widgets from './components/widgets/Widgets'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count ** 3 + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
+    <div className="app">
+      <Sidebar />
+      <Feed />
+      <Widgets />
+    </div>
   )
 }
 
 export default App
+
